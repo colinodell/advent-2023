c385e3d9148d83ecac662c26e7171ee6aa3a00a6
Colin O'Dell <codell@seatgeek.com>
2023-12-20 14:53:48 -0500

    Day 20: Pulse Propagation (part 1)

---
+++ src/main/kotlin/com/colinodell/advent2023/Day20.kt
@@ -0,0 +1,131 @@
+package com.colinodell.advent2023
+
+class Day20(private val input: List<String>) {
+    private enum class Pulse { LOW, HIGH }
+
+    private sealed class Module(val name: String) {
+        open fun onReceive(p: Pulse, from: String) {}
+        open fun sendPulse(lastPulse: Pulse): Pulse? = null
+
+        override fun toString() = "[${this::class.simpleName}] $name"
+
+        class Button : Module("button") {
+            override fun sendPulse(lastPulse: Pulse) = Pulse.LOW
+        }
+
+        class Broadcaster : Module("broadcaster") {
+            override fun sendPulse(lastPulse: Pulse) = lastPulse
+        }
+
+        class FlipFlop(name: String) : Module(name) {
+            private var on = false
+
+            override fun onReceive(p: Pulse, from: String) {
+                if (p == Pulse.LOW) {
+                    on = !on
+                }
+            }
+
+            override fun sendPulse(lastPulse: Pulse) = when {
+                lastPulse == Pulse.HIGH -> null
+                on -> Pulse.HIGH
+                else -> Pulse.LOW
+            }
+        }
+
+        class Conjunction(name: String) : Module(name) {
+            private val lastPulses = mutableMapOf<String, Pulse>()
+
+            fun expectInput(input: String) {
+                lastPulses[input] = Pulse.LOW
+            }
+
+            override fun onReceive(p: Pulse, from: String) = lastPulses.set(from, p)
+
+            override fun sendPulse(lastPulse: Pulse) = if (lastPulses.values.all { it == Pulse.HIGH }) Pulse.LOW else Pulse.HIGH
+        }
+
+        class Untyped(name: String) : Module(name)
+
+        companion object {
+            fun create(name: String) = when {
+                name == "button" -> Button()
+                name == "broadcaster" -> Broadcaster()
+                name.first() == '%' -> FlipFlop(name.drop(1))
+                name.first() == '&' -> Conjunction(name.drop(1))
+                else -> Untyped(name)
+            }
+        }
+    }
+
+    private val receivers = mapOf("button" to listOf("broadcaster")) + input
+        .map { it.split(" -> ") }
+        .associate { (source, targets) -> source.trimStart('&', '%') to targets.split(", ") }
+
+    private val modules = buildMap {
+        // Button always exists
+        this["button"] = Module.Button()
+
+        // Add modules based on the input names
+        input
+            .map { it.split(" -> ") }
+            .map { (source, _) -> Module.create(source) }
+            .forEach { this[it.name] = it }
+
+        // Add any output-only modules
+        receivers.values.flatten().filter { it !in this }.forEach { this[it] = Module.Untyped(it) }
+    }
+
+    init {
+        // Tell conjunctions about their expected inputs
+        receivers
+            .flatMap { (input, receivers) ->
+                receivers
+                    .filter { modules[it] is Module.Conjunction }
+                    .map { input to (modules[it] as Module.Conjunction) }
+            }
+            .forEach { (input, conjunction) ->
+                conjunction.expectInput(input)
+            }
+    }
+
+    private fun pressButton(
+        onPulseReceived: (Module, Pulse) -> Unit = { _, _ -> },
+        onPulseSent: (Module, Pulse) -> Unit = { _, _ -> }
+    ) {
+        val queue = mutableListOf(modules["button"]!! to Pulse.LOW)
+        while (queue.isNotEmpty()) {
+            val (module, receivedPulse) = queue.removeAt(0)
+
+            // Send the pulse
+            val output = module.sendPulse(receivedPulse) ?: continue
+            onPulseSent(module, output)
+
+            // Receive the pulse
+            for (destination in receivers[module.name]!!.map { modules[it]!! }) {
+                destination.onReceive(output, module.name)
+
+                onPulseReceived(destination, output)
+
+                queue.add(destination to output)
+            }
+        }
+    }
+
+    fun solvePart1(): Int {
+        var lowPulses = 0
+        var highPulses = 0
+
+        repeat(1000) {
+            pressButton(onPulseReceived = { _, pulse ->
+                when (pulse) {
+                    Pulse.LOW -> lowPulses++
+                    Pulse.HIGH -> highPulses++
+                }
+            })
+        }
+
+        return lowPulses * highPulses
+    }
+
+}
---
+++ src/test/kotlin/com/colinodell/advent2023/Day20Test.kt
@@ -0,0 +1,28 @@
+package com.colinodell.advent2023
+
+import com.colinodell.advent2023.Inputs.inputAsListOfString
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+@DisplayName("Day 20: Pulse Propagation")
+class Day20Test {
+    private val exampleInput1 = inputAsListOfString("day20_example1.txt")
+    private val exampleInput2 = inputAsListOfString("day20_example2.txt")
+    private val puzzleInput = inputAsListOfString("day20_input.txt")
+
+    @Test
+    fun `Part 1 - Example 1`() {
+        assertThat(Day20(exampleInput1).solvePart1()).isEqualTo(32000000)
+    }
+
+    @Test
+    fun `Part 1 - Example 2`() {
+        assertThat(Day20(exampleInput2).solvePart1()).isEqualTo(11687500)
+    }
+
+    @Test
+    fun `Part 1 - Actual`() {
+        assertThat(Day20(puzzleInput).solvePart1()).isEqualTo(944750144)
+    }
+}
